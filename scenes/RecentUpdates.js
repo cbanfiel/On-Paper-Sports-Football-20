e6cdfe78d20f19ef181fb206a6d117282a799fb7
Chad Banfield <cbanfiel@emich.edu>
2020-02-22 14:51:08 -0500

    2-22

--- scenes/RecentUpdates.js
+++ scenes/RecentUpdates.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { StyleSheet, ScrollView, Text, View } from 'react-native';
-import { Card } from 'react-native-elements';
-import { Actions } from 'react-native-router-flux';
-import { TouchableOpacity } from 'react-native';
 import Background from '../components/background';
-import ListItem from '../components/ListItem';
 
 
 const updateLog = [
+    {
+        date: '1/31/2020',
+        updates: "-Reduced initial coach rating disparity"
+    },
     {
         date: '12/1/2019',
         updates: "-Added second chances in recruiting, you can use second chances to try and flip recruits that have committed to other schools\n-Schedule strength now plays a role in recruiting, stronger schedules attract better recruits\n-Recruit rating tweaks (lower end qb/rb recruits are slightly better)"
